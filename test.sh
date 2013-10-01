bf89eac33623798e720f5f65b76a9298ada59be5
Oliver Matthews <oliver@codersoffortune.net>
2013-10-01 16:57:06 +0100

    basic test env

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+rm -r test
+mkdir test
+cd test
+
+cat >> first.md << EOF
+This is the [[first]] file, to be followed by the [[second]] one.
+EOF
+
+cat >> second.md << EOF
+Now the [[second]] file.
+
+Previously there was the [[first]]; soon there will be a [[third]].
+EOF
+
+vim -S ../plugin/nvim.vim -c 'exec NVIM_init()' -c 'py nvimdb.rebuild_database()'
